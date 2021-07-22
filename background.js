041bc72464d3cb3b0eb6486a0c724ccd97f27410
mdestafadilah <desta.08b@gmail.com>
2021-07-22 14:10:45 +0700

    Introduce Manifest - Open DevTools for See what hapen on there.

---
+++ background.js
@@ -0,0 +1,21 @@
+alert('hi')
+
+function MyGenericClick(info, tab){
+    console.log("Clicked on page: ", info, tab)
+}
+
+function MyImageClick(info, tab) {
+    console.log("Clicked an image:", info, tab);
+}
+
+chrome.contextMenus.create({
+    "title": "Share",
+    "contexts" : ["page"],
+    "onclick": MyGenericClick,
+})
+
+chrome.contextMenus.create({
+    "title": "Share",
+    "contexts" : ["page"],
+    "onclick": MyImageClick,
+})
