95731fca0d42be942644d5d533ac3fa9473a760d
mdestafadilah <desta.08b@gmail.com>
2021-08-04 13:53:57 +0700

    Finish with Note
    Very not clear and no updated.

--- content.js
+++ content.js
@@ -1,1 +1,21 @@
-console.log('hi')
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if(request.action === 'get_images"') {
+
+        console.log(request);
+
+        let formated_image = [];
+
+        let images = $('img').map((i,img) => {
+            if($(img).data('src')){
+                return {
+                    src: $(img).data('src')
+                }
+            } else if($(img).src) {
+                return {
+                    src: $(img).src
+                }
+            }
+        })
+        sendResponse(images);
+    }
+})
--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,10 @@
-console.log('hi')
+chrome.tabs.query({
+    active: true,
+    currentWindow: true
+},(tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, {
+        action: "get_images"
+    }, response => {
+        console.log(response)
+    })
+})
